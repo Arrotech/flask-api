f19426bd9220a300c070372d4e47fd75cd5bf8ce
Harun Gachanja <arrotechdesign@gmail.com>
2021-06-14 17:38:36 +0300

    redirect user to success page

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import os
 
+from flask import make_response, jsonify
+
 from app import create_app
 
 
 app = create_app(os.environ.get('FLASK_ENV'))
+
+
+@app.route('/')
+def home():
+    return make_response(jsonify({
+        "message": "OK",
+        "status": "200"
+    }), 200)
+
 
 if __name__ == '__main__':
     app.run()
