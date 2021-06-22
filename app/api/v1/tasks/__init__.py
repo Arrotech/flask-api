3591b5e1f8a43d391994380ccf763ecaf77ecfaf
Harun Gachanja <arrotechdesign@gmail.com>
2021-06-22 11:50:04 +0300

    improve test coverage

--- app/api/v1/tasks/__init__.py
+++ app/api/v1/tasks/__init__.py
@@ -1,9 +1,1 @@
-import os
-from app import create_app
-from instance.celeryconfig import make_celery
-
-app = create_app(os.environ.get('FLASK_ENV'))
-celery = make_celery(app)
-
 # write your tasks here
-# create a task and use the @celery decorator 
