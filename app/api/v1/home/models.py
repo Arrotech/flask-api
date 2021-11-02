bedda80aee375322dc4a0a6a9ebe502b2ab4f846
Harun Gitundu <hgitundu@fintech-group.com>
2021-11-02 20:16:40 +0300

    fix docker container

--- app/api/v1/home/models.py
+++ app/api/v1/home/models.py
@@ -1,3 +1,11 @@
 from app.extensions import db
 
 # write your models here
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
